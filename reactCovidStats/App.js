aa4a54404d28440d8ca798d5b3794ebe97b5fd29
Peter Irlam <irlampeter@gmail.com>
2021-08-28 14:29:03 +0100

    Create App.js

---
+++ reactCovidStats/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import CovidStats from './CovidStats';
+
+function App() {
+  return (
+    <div className="App">
+     <CovidStats />
+    </div>
+  );
+}
+
+export default App;
